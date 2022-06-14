035e01a2655adf4040c00269de3d8300e391da4f
Felkng <105019177+Felkng@users.noreply.github.com>
2022-06-14 09:12:46 -0300

    Create 9.24-Fibonacci.c

---
+++ 9.24-Fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int in, serie, ct_loop, add, change=1;
+    scanf("%d",&in);
+    for(serie=1,ct_loop=0, add=0;ct_loop < in; ct_loop++)
+    {
+            switch(change)
+            {
+                case 1:
+                    add+=serie;
+                    printf("%d ",serie);
+                    change = 2;
+                continue;
+                case 2:
+                    serie += add;
+                    printf("%d ", add);
+                    change = 1;
+            }
+    }
+
+}
